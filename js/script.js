8f360d358a69e1063027d17443321ea1c3202636
SwitchTia <oblasser.tatiana@gmail.com>
2025-11-14 15:46:08 +0100

    Feat: wip

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 // Attraverso l'apposita API di Boolean
 // https://flynn.boolean.careers/exercises/api/random/mail
 // generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+
+
+for (let i = 0; i <= 10; i++){
+    const apiURL = "https://flynn.boolean.careers/exercises/api/random/mail";
+
+    axios
+    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then(function(resp){
+        const randomEmail = resp.data.response;
+    
+    })
+}
